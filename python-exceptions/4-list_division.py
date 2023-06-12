474518a0030e12e3d8d7a3ca51b2a88d31f2cf12
Macj04 <miguelcoa49@gmail.com>
2023-06-11 18:13:56 -0700

    task 4 finish

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    num = 0
+    i = 0
+    while i < list_length:
+        try:
+            num = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            num = 0
+            print('wrong type')
+        except ZeroDivisionError:
+            num = 0
+            print('division by 0')
+        except IndexError:
+            num = 0
+            print('out of range')
+        finally:
+            print('', end='')
+        new_list.append(num)
+        i += 1
+    return new_list
