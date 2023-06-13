68155394735b521cfb59dbd10ef2b1a06548f3bc
Macj04 <miguelcoa49@gmail.com>
2023-06-13 10:35:19 -0700

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Write a class Square that defines a square'''
+
+
+class Square:
+    '''square class'''
+
+
+    def __init__(self, size=0):
+        self.set_size(size)
+
+
+    def set_size(self, size):
+        if not (type(size) == int):
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = size
