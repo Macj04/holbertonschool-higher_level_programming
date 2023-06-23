cd5c3d5efc8b13d1ff8bc8073f32d1db44b46a23
Macj04 <miguelcoa49@gmail.com>
2023-06-23 16:12:26 -0700

    fix task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,6 @@
 
 
 def is_kind_of_class(obj, a_class):
-    is_instance = isinstance(obj, a_class)
-    is_subclass = issubclass(type(obj), a_class)
-    return is_instance or is_subclass
+    ''' Function that return '''
+    ins = (isinstance(obj, a_class))
+    return ins
